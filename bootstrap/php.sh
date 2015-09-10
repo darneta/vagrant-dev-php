65c1812322d5378b69bf502d4673581055b5cc99
Darius Krištapavičius <darius@cuf.lt>
2015-09-10 21:16:38 +0300

    move some php.ini configs to config.yml

--- bootstrap/php.sh
+++ bootstrap/php.sh
@@ -14,8 +14,10 @@ mv composer.phar /usr/local/bin/composer
 yum install -y gcc gcc-c++ autoconf automake
 pecl install Xdebug
 echo -e "[xdebug]\nzend_extension=\"/usr/lib64/php/modules/xdebug.so\"\nxdebug.remote_enable = 1\nxdebug.remote_connect_back = 1\nxdebug.idekey = \"vagrant\"" >> /etc/php.ini
+sed -i 's/display_errors = .*/display_errors = On/' /etc/php.ini
+sed -i 's/display_startup_errors = .*/display_startup_errors = On/' /etc/php.ini
 
 # Install blackfire.io
 yum -y install pygpgme
-wget -O - "http://packages.blackfire.io/fedora/blackfire.repo" | sudo tee /etc/yum.repos.d/blackfire.repo
+wget -O - "http://packages.blackfire.io/fedora/blackfire.repo" | tee /etc/yum.repos.d/blackfire.repo
 yum -y install blackfire-agent
